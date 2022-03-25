5011986a594dc98908dadbf5f8f9f65456db909b
Omar Malaeb <omarmlb002@gmail.com>
2022-03-25 10:45:45 +0200

    url of the website and the api

--- Server/get_rate.php
+++ Server/get_rate.php
@@ -1,3 +1,7 @@
 <?php
 
+// https://lirarate.org/ (the url of the website)
+
+// https://lirarate.org/wp-json/lirarate/v2/rates?currency=LBP (the url of the api that the website is calling to get the rate)
+
 ?>
