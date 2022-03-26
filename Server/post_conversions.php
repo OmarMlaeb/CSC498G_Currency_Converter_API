0b11bf0c56f1356e60e4ffbb59f269927ac364bc
Omar Malaeb <omarmlb002@gmail.com>
2022-03-26 19:24:04 +0200

    created lists for each parameter needed to be posted

--- Server/post_conversions.php
+++ Server/post_conversions.php
@@ -2,4 +2,10 @@
 
 include("db_info.php");
 
+$amount = $_POST["amount"]; // list that contains all the amounts sent from the front-end
+$rate = $_POST["rate"]; // list that contains all the rates sent from the front-end
+$converted_from = $_POST["converted_from"]; // list that contains all the strings (USD or LBP) sent from the front-end
+$converted_to = $_POST["converted_to"]; // list that contains all the strings (USD or LBP) sent from the front-end
+$converted_amount = $_POST["converted_amount"]; // list that contains all the converted amounts sent from the front-end
+
 ?>
