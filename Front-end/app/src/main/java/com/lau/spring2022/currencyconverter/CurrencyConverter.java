1878c1b80c6649e612e0c68b43553de146a8f7bc
Omar Malaeb <omarmlb002@gmail.com>
2022-04-02 12:41:03 +0300

    added array currencies & connected adapter to array & connected spinner to adapter

--- Front-end/app/src/main/java/com/lau/spring2022/currencyconverter/CurrencyConverter.java
+++ Front-end/app/src/main/java/com/lau/spring2022/currencyconverter/CurrencyConverter.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
@@ -37,6 +38,16 @@ public class CurrencyConverter extends AppCompatActivity {
         sp2 = (Spinner) findViewById(R.id.spinner_bar2);
         convert = (Button) findViewById(R.id.convert);
         result = (TextView) findViewById(R.id.result);
+
+        // spinner to choose the currency
+        String[] from = {"USD", "LBP"};
+        ArrayAdapter<String> adp1 = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, from);
+        sp1.setAdapter(adp1);
+
+        // spinner to choose the currency
+        String[] to = {"LBP", "USD"};
+        ArrayAdapter<String> adp2 = new ArrayAdapter<>(this,  androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, to);
+        sp2.setAdapter(adp2);
     }
 
     // to calculate the conversion of the amount
