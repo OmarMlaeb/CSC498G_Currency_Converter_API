f929564b0698fadf458318782585c29e127ef2c1
Omar Malaeb <omarmlb002@gmail.com>
2022-04-03 13:46:55 +0300

    updated post php file

--- Server/post_conversions.php
+++ Server/post_conversions.php
@@ -2,11 +2,12 @@
 
 include("db_info.php");
 
-$amount = $_POST["amount"]; // list that contains all the amounts sent from the front-end
-$rate = $_POST["rate"]; // list that contains all the rates sent from the front-end
-$converted_from = $_POST["converted_from"]; // list that contains all the strings (USD or LBP) sent from the front-end
-$converted_to = $_POST["converted_to"]; // list that contains all the strings (USD or LBP) sent from the front-end
-$converted_amount = $_POST["converted_amount"]; // list that contains all the converted amounts sent from the front-end
+// $_GET to collect form data after submitting
+$amount = $_GET["amount"]; // list that contains all the amounts sent from the front-end
+$rate = $_GET["rate"]; // list that contains all the rates sent from the front-end
+$converted_from = $_GET["converted_from"]; // list that contains all the strings (USD or LBP) sent from the front-end
+$converted_to = $_GET["converted_to"]; // list that contains all the strings (USD or LBP) sent from the front-end
+$converted_amount = $_GET["converted_amount"]; // list that contains all the converted amounts sent from the front-end
 
 $query = $mysqli->prepare("INSERT INTO conversions (amount, rate, converted_from, converted_to, converted_amount) VALUES (?, ?, ?, ?, ?)"); // to insert values to the table "conversions" that the user will be converting on the app 
 // the values are "?" to prevent sql injections
